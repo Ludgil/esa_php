500b3dbcac1e732a0290cbf6f1b6d8e3cd9d6b65
Ludgil <43419798+Ludgil@users.noreply.github.com>
2024-04-09 22:09:01 +0200

    Add files via upload

--- damier/index.php
+++ damier/index.php
@@ -1,25 +1,41 @@
-<?php
-echo '<table style="border:1px solid black; border-collapse: collapse;">';
-for($i=0;$i<8;$i++){
-    echo '<tr>';
-    for($j=0;$j<8;$j++){
-        if($i % 2){
-            if(!($j % 2)){
-                echo '<td style="background-color:grey; padding:20px; margin:0"></td>';
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Damier</title>
+</head>
+<body>
+    <div class="container">
+    <?php
+    echo '<table class="damier">';
+    for($i=0;$i<8;$i++){
+        echo '<tr>';
+        for($j=0;$j<8;$j++){
+            if($i % 2){
+                if(!($j % 2)){
+                    echo '<td class="tile color1"></td>';
+                }
+                else{
+                    echo '<td class="tile color2"></td>';
+                }
+            }else{
+                if(($j % 2)){
+                    echo '<td class="tile color1"></td>';
+                }
+                else{
+                    echo '<td class="tile color2"></td>';
+                }
             }
-            else{
-                echo '<td style="background-color:white; padding:20px; margin:0"></td>';
-            }
-        }else{
-            if(($j % 2)){
-                echo '<td style="background-color:grey; padding:20px; margin:0"></td>';
-            }
-            else{
-                echo '<td style="background-color:white; padding:20px; margin:0"></td>';
-            }
+            
         }
-        
+        echo '</tr>';
     }
-    echo '</tr>';
-}
-echo '</table>';
+    echo '</table>';
+    ?>
+    </div>
+</body>
+</html>
+
+
