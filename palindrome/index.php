a45956d6266ecc26e4b6393a0f5e630fb92f8be1
Ludgil <gilotaux.lud@gmail.com>
2024-04-20 17:57:58 +0200

    palindrome exo

---
+++ palindrome/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$mot = readline("Entrer un mot : ");
+$count = intdiv(strlen($mot), 2);
+$palindrome = false;
+for ($i=0; $i < $count ; $i++) { 
+    if ($mot[$i] != $mot[strlen($mot) - 1 - $i]){
+        $palindrome = false;
+    }else{
+        $palindrome = true;
+    }
+}
+
+if ($palindrome){
+    echo "Le mot ". $mot ." est un palindrome \n";
+}else{
+    echo "Le mot ". $mot ." n'est pas un palindrome \n";
+}
+
+
