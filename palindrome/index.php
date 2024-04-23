2990917a628b1d4b815a8c0169c0a41f30cd9b91
Ludgil <gilotaux.lud@gmail.com>
2024-04-23 18:13:10 +0200

    strrev

--- palindrome/index.php
+++ palindrome/index.php
@@ -1,17 +1,25 @@
 <?php
 
 $mot = readline("Entrer un mot : ");
-$count = intdiv(strlen($mot), 2);
-$palindrome = false;
-for ($i=0; $i < $count ; $i++) { 
-    if ($mot[$i] != $mot[strlen($mot) - 1 - $i]){
-        $palindrome = false;
-    }else{
-        $palindrome = true;
-    }
-}
+// $count = intdiv(strlen($mot), 2);
+// $palindrome = false;
+// for ($i=0; $i < $count ; $i++) { 
+//     if ($mot[$i] != $mot[strlen($mot) - 1 - $i]){
+//         $palindrome = false;
+//     }else{
+//         $palindrome = true;
+//     }
+// }
 
-if ($palindrome){
+// if ($palindrome){
+//     echo "Le mot ". $mot ." est un palindrome \n";
+// }else{
+//     echo "Le mot ". $mot ." n'est pas un palindrome \n";
+// }
+
+$reverse = strrev($mot);
+
+if ($mot === $reverse){
     echo "Le mot ". $mot ." est un palindrome \n";
 }else{
     echo "Le mot ". $mot ." n'est pas un palindrome \n";
